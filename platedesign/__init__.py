53ba63a05067e4f2459491870f2cc53cb9c05b09
Sebastian Castillo-Hair <castillohair@gmail.com>
2017-06-25 15:23:26 -0500

    Removed support for pandas<0.20.

--- platedesign/__init__.py
+++ platedesign/__init__.py
@@ -8,24 +8,12 @@ Tools for designing plate experiments
 # https://packaging.python.org/en/latest/single_source_version.html
 __version__ = '0.4.0'
 
-import packaging
-import packaging.version
-
 from . import experiment
 from . import inducer
 from . import plate
 
 # Change pandas header style
 import pandas
-if packaging.version.parse(pandas.__version__) \
-        < packaging.version.parse('0.18'):
-    format_module = pandas.core.format
-elif packaging.version.parse(pandas.__version__) \
-        < packaging.version.parse('0.20'):
-    format_module = pandas.formats.format
-else:
-    import pandas.io.formats.excel
-    format_module = pandas.io.formats.excel
-
-format_module.header_style = {"font": {"bold": True,
-                                       "color": "FF1F497D"}}
+import pandas.io.formats.excel
+pandas.io.formats.excel.header_style = {"font": {"bold": True,
+                                                 "color": "FF1F497D"}}
