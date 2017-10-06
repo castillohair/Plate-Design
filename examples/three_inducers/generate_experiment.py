a60427ec40f8eb9e033ea1a56e200606708455e3
Sebastian Castillo-Hair <castillohair@gmail.com>
2017-10-06 18:09:42 -0500

    Changed names of randomization parameters in Experiment.

--- examples/three_inducers/generate_experiment.py
+++ examples/three_inducers/generate_experiment.py
@@ -3,8 +3,8 @@ import platedesign
 
 exp = platedesign.experiment.Experiment()
 exp.n_replicates = 5
-exp.randomize_inducer = True
-exp.randomize_plate = True
+exp.randomize_inducers = True
+exp.randomize_plates = True
 exp.plate_locations = ['Stack 1-1', 'Stack 1-2', 'Stack 1-3', 'Stack 1-4']
 exp.measurement_template = '../supporting_files/template_FlowCal.xlsx'
 
