d66b1829659231b37a57982098cece9162d23d6a
Sebastian Castillo-Hair <castillohair@gmail.com>
2017-12-13 22:13:45 -0600

    Made examples more uniform.

--- examples/one_inducer/generate_experiment.py
+++ examples/one_inducer/generate_experiment.py
@@ -2,15 +2,21 @@
 import platedesign
 
 exp = platedesign.experiment.Experiment()
-exp.n_replicates = 5
+exp.n_replicates = 3
+exp.n_replicates_extra_inducer = 2
 exp.plate_resources['Location'] = ['Stack 1-1',
                                    'Stack 1-2',
                                    'Stack 1-3',
-                                   'Stack 1-4']
+                                   'Stack 1-4',
+                                   'Stack 2-1',
+                                   'Stack 2-2',
+                                   'Stack 2-3',
+                                   'Stack 2-4']
 exp.randomize_inducers = True
 exp.randomize_plate_resources = True
 exp.measurement_template = '../supporting_files/template_FlowCal.xlsx'
 exp.measurement_order = 'Location'
+exp.plate_measurements = ['Final OD600', 'Incubation time (min)']
 exp.replicate_measurements = ['Date', 'Run by']
 
 # Inducers
@@ -28,22 +34,12 @@ exp.add_inducer(iptg)
 plate = platedesign.plate.Plate('P1', n_rows=4, n_cols=6)
 plate.cell_strain_name = 'Test Strain 1'
 plate.samples_to_measure = 12
-plate.total_media_vol = 16000.
-plate.sample_media_vol = 500.
-plate.cell_setup_method = 'fixed_volume'
-plate.cell_predilution = 100
-plate.cell_predilution_vol = 1000
-plate.cell_shot_vol = 5
 plate.apply_inducer(inducer=iptg, apply_to='wells')
 exp.add_plate(plate)
 
 plate = platedesign.plate.Plate('P2', n_rows=4, n_cols=6)
 plate.cell_strain_name = 'Test Strain 2'
 plate.samples_to_measure = 12
-plate.total_media_vol = 16000.
-plate.sample_media_vol = 500.
-plate.cell_setup_method = 'fixed_volume'
-plate.cell_shot_vol = 5
 plate.apply_inducer(inducer=iptg, apply_to='wells')
 exp.add_plate(plate)
 
@@ -51,12 +47,15 @@ exp.add_plate(plate)
 plate = platedesign.plate.Plate('P3', n_rows=4, n_cols=6)
 plate.cell_strain_name = 'Autofluorescence Control Strain'
 plate.samples_to_measure = 4
-plate.total_media_vol = 16000.
-plate.sample_media_vol = 500.
-plate.cell_setup_method = 'fixed_od600'
-plate.cell_predilution = 100
-plate.cell_predilution_vol = 1000
-plate.cell_initial_od600 = 1e-5
 exp.add_plate(plate)
+
+# Specify media and cell setup parameters
+for plate in exp.plates:
+    plate.total_media_vol = 16000.
+    plate.sample_media_vol = 500.
+    plate.cell_setup_method = 'fixed_od600'
+    plate.cell_predilution = 100
+    plate.cell_predilution_vol = 300
+    plate.cell_initial_od600 = 1e-5
 
 exp.generate()
--- examples/plate_array/generate_experiment.py
+++ examples/plate_array/generate_experiment.py
@@ -2,7 +2,8 @@
 import platedesign
 
 exp = platedesign.experiment.Experiment()
-exp.n_replicates = 5
+exp.n_replicates = 3
+exp.n_replicates_extra_inducer = 2
 exp.plate_resources['Location'] = ['Stack 1-1',
                                    'Stack 1-2',
                                    'Stack 1-3',
@@ -10,17 +11,13 @@ exp.plate_resources['Location'] = ['Stack 1-1',
                                    'Stack 2-1',
                                    'Stack 2-2',
                                    'Stack 2-3',
-                                   'Stack 2-4',
-                                   'Stack 3-1',
-                                   'Stack 3-2',
-                                   'Stack 3-3',
-                                   'Stack 3-4',
-                                   ]
+                                   'Stack 2-4']
 exp.randomize_inducers = True
 exp.randomize_plate_resources = True
 exp.measurement_template = '../supporting_files/template_FlowCal.xlsx'
 exp.measurement_order = 'Location'
 exp.plate_measurements = ['Final OD600', 'Incubation time (min)']
+exp.replicate_measurements = ['Date', 'Run by']
 
 # Inducers
 iptg = platedesign.inducer.ChemicalInducer(name='IPTG', units=u'µM')
@@ -28,7 +25,7 @@ iptg.stock_conc = 1e6
 iptg.shot_vol = 5.
 iptg.set_gradient(min=0.5,
                   max=500,
-                  n=16,
+                  n=8,
                   scale='log',
                   use_zero=True)
 exp.add_inducer(iptg)
@@ -44,19 +41,12 @@ xyl.set_gradient(min=5e-3,
                  use_zero=True)
 exp.add_inducer(xyl)
 
-# Plate for autofluorescence control strain
-plate = platedesign.plate.Plate('P9', n_rows=4, n_cols=6)
-plate.cell_strain_name = 'Autofluorescence Control Strain'
-plate.samples_to_measure = 4
-plate.total_media_vol = 16000.
-exp.add_plate(plate)
-
-# Plate array
+# Plate array for test strain
 platearray = platedesign.plate.PlateArray(
     'PA1',
-    array_n_rows=4,
+    array_n_rows=2,
     array_n_cols=2,
-    plate_names=['P{}'.format(i+1) for i in range(8)],
+    plate_names=['P{}'.format(i+1) for i in range(4)],
     plate_n_rows=4,
     plate_n_cols=6)
 platearray.cell_strain_name = 'Test Strain 1'
@@ -65,12 +55,19 @@ platearray.apply_inducer(inducer=xyl, apply_to='rows')
 platearray.apply_inducer(inducer=iptg, apply_to='cols')
 exp.add_plate(platearray)
 
-# Add common settings to plates
+# Plate for autofluorescence control strain
+plate = platedesign.plate.Plate('P9', n_rows=4, n_cols=6)
+plate.cell_strain_name = 'Autofluorescence Control Strain'
+plate.samples_to_measure = 4
+plate.total_media_vol = 16000.
+exp.add_plate(plate)
+
+# Specify media and cell setup parameters
 for plate in exp.plates:
     plate.sample_media_vol = 500.
-    plate.cell_setup_method = 'fixed_od600'
+    plate.cell_setup_method = 'fixed_dilution'
     plate.cell_predilution = 100
-    plate.cell_predilution_vol = 1000
-    plate.cell_initial_od600 = 1e-5
+    plate.cell_predilution_vol = 300
+    plate.cell_total_dilution = 1e5
 
 exp.generate()
